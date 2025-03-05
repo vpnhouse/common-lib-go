ce0b4e9d7ee1b3af280a2aaeb40d5bdea984bab3
Sergey Kovalev <xray@randoman.ru>
2025-03-05 14:09:09 +0700

    103254 Implement connection manager (#9)
    * Add in-memory data pipe
    
    * Add missing mutex unlocks
    
    * Add deadlock test, single-byte transfer test, remove debugging
    
    * Implement connections manager
    
    * Refactor connmgr to be lockless
    
    * Replace Close() with Unregister()
    
    * Protect connid with mutex
    
    * Add more information for xpipe unit test
    
    * Remove xpipe from this PR

---
+++ connmgr/connmgr_test.go
@@ -0,0 +1,111 @@
+package connmgr
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	testMaxConns = 1024 * 1024
+)
+
+var mgr = NewConnMgr()
+
+type conn struct {
+	id     int
+	closed bool
+}
+
+// Close implements net.Conn.
+func (c *conn) Close() error {
+	c.closed = true
+	return nil
+}
+
+// LocalAddr implements net.Conn.
+func (c *conn) LocalAddr() net.Addr {
+	panic("unimplemented")
+}
+
+// Read implements net.Conn.
+func (c *conn) Read(b []byte) (n int, err error) {
+	panic("unimplemented")
+}
+
+// RemoteAddr implements net.Conn.
+func (c *conn) RemoteAddr() net.Addr {
+	panic("unimplemented")
+}
+
+// SetDeadline implements net.Conn.
+func (c *conn) SetDeadline(t time.Time) error {
+	panic("unimplemented")
+}
+
+// SetReadDeadline implements net.Conn.
+func (c *conn) SetReadDeadline(t time.Time) error {
+	panic("unimplemented")
+}
+
+// SetWriteDeadline implements net.Conn.
+func (c *conn) SetWriteDeadline(t time.Time) error {
+	panic("unimplemented")
+}
+
+// Write implements net.Conn.
+func (c *conn) Write(b []byte) (n int, err error) {
+	panic("unimplemented")
+}
+
+func TestRegister(t *testing.T) {
+
+	conns := make([]*conn, 0)
+
+	for idx := 0; idx < testMaxConns; idx++ {
+		c := &conn{}
+		id, err := mgr.Register(c)
+		assert.Nil(t, err)
+		assert.GreaterOrEqual(t, id, connMinimalID)
+		c.id = id
+		conns = append(conns, c)
+	}
+
+	for _, c := range conns {
+		mgrc, err := mgr.Lookup(c.id)
+		assert.Nil(t, err)
+		assert.Equal(t, c.id, mgrc.(*conn).id)
+	}
+
+	for _, c := range conns {
+		err := mgr.Unregister(c.id)
+		assert.Nil(t, err)
+
+		err = mgr.Unregister(c.id)
+		assert.ErrorIs(t, ErrNotExist, err)
+	}
+
+}
+
+func TestCloseAll(t *testing.T) {
+	conns := make([]*conn, 0)
+
+	for idx := 0; idx < testMaxConns; idx++ {
+		c := &conn{}
+		id, err := mgr.Register(c)
+		assert.Nil(t, err)
+		assert.GreaterOrEqual(t, id, connMinimalID)
+		c.id = id
+		conns = append(conns, c)
+	}
+
+	mgr.CloseAll()
+
+	for _, c := range conns {
+		_, err := mgr.Lookup(c.id)
+		assert.ErrorIs(t, ErrNotExist, err)
+		assert.True(t, c.closed)
+	}
+}
