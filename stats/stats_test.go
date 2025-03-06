10bc30d523acc98d70e764b9a3cbaa624a75866a
Artem Alemasov <artem.alemasov@gmail.com>
2025-03-06 21:29:34 +0800

    107979 Add more tests

--- stats/stats_test.go
+++ stats/stats_test.go
@@ -52,3 +52,39 @@ func TestService(t *testing.T) {
 	assert.Equal(t, uint64(456)*10, report.DeltaTx)
 	assert.True(t, report.DeltaT > 0)
 }
+
+func TestServiceEmptyData(t *testing.T) {
+	var reports []*Report
+	s, err := New(time.Second, func(report *Report) {
+		reports = append(reports, report)
+	})
+	assert.NoError(t, err)
+
+	sessionID := uuid.New()
+	session := &Session{}
+
+	numMetadataCalls := 0
+	metadata := func(sessionID uuid.UUID) *Session {
+		numMetadataCalls++
+		session.SessionID = sessionID.String()
+		return session
+	}
+
+	for i := 0; i < 10; i++ {
+		s.ReportStats(sessionID, 123, 456, metadata)
+	}
+
+	// Metadata must be called 1 time for the same SessionID
+	assert.Equal(t, 1, numMetadataCalls)
+
+	time.Sleep(time.Second * 2)
+
+	assert.Equal(t, 1, len(reports))
+	report := reports[0]
+	assert.Equal(t, sessionID.String(), report.SessionID)
+	assert.Empty(t, report.InstallationID)
+	assert.Empty(t, report.UserID)
+	assert.Equal(t, uint64(123)*10, report.DeltaRx)
+	assert.Equal(t, uint64(456)*10, report.DeltaTx)
+	assert.True(t, report.DeltaT > 0)
+}
