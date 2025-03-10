2f99712400f46695e5ac8b0e2afce6ff0b6128f6
Artem Alemasov <artem.alemasov@gmail.com>
2025-03-10 20:39:22 +0800

    107979 Refactor to use generics

--- stats/service.go
+++ stats/service.go
@@ -1,29 +1,29 @@
 package stats
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/vpnhouse/common-lib-go/xcache"
-	"github.com/vpnhouse/common-lib-go/xutils"
 )
 
 const maxBytes = 32 << 20 // 32 Mb
 
-type Service struct {
+type Service[T any] struct {
 	sessions *xcache.Cache // session_id -> rx, tx, timestamp, session {session_id, installation_id, user_id}
-	onFlush  OnFlush
+	onFlush  OnFlush[T]
 	extra    Extra
 }
 
-type Session struct {
-	SessionID      string // uuid
-	InstallationID string // uuid
-	UserID         string // project(uuid)/auth_method(uuid)/ref(firebase|keycloak id)
+type Session[T any] struct {
+	SessionID string // uuid
+	Data      *T
 }
 
-type Report struct {
-	Session
+type Report[T any] struct {
+	Session[T]
+	Created uint64
 	DeltaRx uint64
 	DeltaTx uint64
 	DeltaT  uint64
@@ -31,17 +31,18 @@ type Report struct {
 
 type Extra map[string]string
 
-type OnFlush func(report *Report, extra Extra)
+type OnFlush[T any] func(report *Report[T], extra Extra)
 
 var Now func() uint64 = func() uint64 {
 	return uint64(time.Now().Unix())
 }
 
-func parse(k []byte, v []byte, nowT uint64) *Report {
+func parse[T any](k []byte, v []byte, nowT uint64) *Report[T] {
 	sessionID, _ := uuid.FromBytes(k)
-	r := &Report{
-		Session: Session{
+	r := &Report[T]{
+		Session: Session[T]{
 			SessionID: sessionID.String(),
+			Data:      new(T),
 		},
 	}
 	i := 0
@@ -55,30 +56,24 @@ func parse(k []byte, v []byte, nowT uint64) *Report {
 	i += 8
 	r.DeltaT = nowT - startT
 
-	// Installation ID
-	installationIDLen := int(ParseUint16(v[i : i+2]))
+	// Data
+	dataLen := int(ParseUint16(v[i : i+2]))
 	i += 2
-	r.InstallationID = xutils.BytesToString(v[i : i+installationIDLen])
-	i += installationIDLen
-
-	// User ID
-	userIDLen := int(ParseUint16(v[i : i+2]))
-	i += 2
-	r.UserID = xutils.BytesToString(v[i : i+userIDLen])
+	// Must not be any error
+	_ = json.Unmarshal(v[i:i+dataLen], r.Data)
 
 	return r
 }
 
-func toValue(session *Session, drx, dtx uint64) []byte {
+func toValue[T any](session *Session[T], drx, dtx uint64) []byte {
 	// [8] Rx +
 	// [8] Tx +
 	// [8] StartT +
-	// [2] len(InstallationID) +
-	// [.] len(InstallationID) +
-	// [2] len(UserID) +
-	// [.] len(UserID)
+	// [2] len(Data) +
+	// [.] Data
 
-	d := make([]byte, 8+8+8+2+len(session.InstallationID)+2+len(session.UserID))
+	data, _ := json.Marshal(session.Data)
+	d := make([]byte, 8+8+8+2+len(data))
 
 	i := 0
 	// Delta Rx, Tx
@@ -90,24 +85,17 @@ func toValue(session *Session, drx, dtx uint64) []byte {
 	SetUint64(now, d[i:i+8])
 	i += 8
 
-	// Installation ID
-	installationIDLen := len(session.InstallationID)
-	SetUint16(uint16(installationIDLen), d[i:i+2])
+	// Data
+	dataLen := len(data)
+	SetUint16(uint16(dataLen), d[i:i+2])
 	i += 2
-	copy(d[i:i+installationIDLen], xutils.StringToBytes(session.InstallationID))
-	i += installationIDLen
-
-	// User ID
-	userIDLen := len(session.UserID)
-	SetUint16(uint16(userIDLen), d[i:i+2])
-	i += 2
-	copy(d[i:i+userIDLen], xutils.StringToBytes(session.UserID))
+	copy(d[i:i+dataLen], data)
 
 	return d
 }
 
-func New(flushInterval time.Duration, onFlush OnFlush, extra Extra) (*Service, error) {
-	s := &Service{
+func New[T any](flushInterval time.Duration, onFlush OnFlush[T], extra Extra) (*Service[T], error) {
+	s := &Service[T]{
 		onFlush: onFlush,
 		extra:   extra,
 	}
@@ -120,17 +108,20 @@ func New(flushInterval time.Duration, onFlush OnFlush, extra Extra) (*Service, e
 	return s, nil
 }
 
-func (s *Service) ReportStats(sessionID uuid.UUID, drx, dtx uint64, metadata func(sessionID uuid.UUID) *Session) {
+func (s *Service[T]) ReportStats(sessionID uuid.UUID, drx, dtx uint64, data func(sessionID uuid.UUID) *T) {
 	s.sessions.Update(sessionID[:], func(v []byte) ([]byte, bool, error) {
 		if len(v) == 0 {
-			session := metadata(sessionID)
-			return toValue(session, drx, dtx), true, nil
+			data := data(sessionID)
+			return toValue(&Session[T]{
+				SessionID: sessionID.String(),
+				Data:      data,
+			}, drx, dtx), true, nil
 		}
 		return AddRxTx(drx, dtx, v), false, nil
 	})
 }
 
-func (s *Service) run(flushInterval time.Duration) {
+func (s *Service[T]) run(flushInterval time.Duration) {
 	ticker := time.NewTicker(flushInterval)
 	for range ticker.C {
 		// It causes the onEvict been called if any
@@ -138,10 +129,10 @@ func (s *Service) run(flushInterval time.Duration) {
 	}
 }
 
-func (s *Service) onEvict(evicted *xcache.Items) {
+func (s *Service[T]) onEvict(evicted *xcache.Items) {
 	now := Now()
 	for i := range evicted.Values {
-		r := parse(evicted.Keys[i], evicted.Values[i], now)
+		r := parse[T](evicted.Keys[i], evicted.Values[i], now)
 		s.onFlush(r, s.extra)
 	}
 }
--- stats/stats_test.go
+++ stats/stats_test.go
@@ -10,11 +10,16 @@ import (
 	"github.com/vpnhouse/common-lib-go/xrand"
 )
 
+type testData struct {
+	InstallationID string `json:"installation_id"`
+	UserID         string `json:"user_id"`
+}
+
 func TestService(t *testing.T) {
-	var reports []*Report
+	var reports []*Report[testData]
 	s, err := New(
 		time.Second,
-		func(report *Report, extra Extra) {
+		func(report *Report[testData], extra Extra) {
 			reports = append(reports, report)
 			assert.Nil(t, extra)
 		},
@@ -23,24 +28,25 @@ func TestService(t *testing.T) {
 	assert.NoError(t, err)
 
 	sessionID := uuid.New()
-	session := &Session{
-		InstallationID: "installation_id_123",
-		UserID: strings.Join([]string{
-			uuid.New().String(),
-			uuid.New().String(),
-			xrand.RandomString(128),
-		}, "/"),
+	session := &Session[testData]{
+		Data: &testData{
+			InstallationID: "installation_id_123",
+			UserID: strings.Join([]string{
+				uuid.New().String(),
+				uuid.New().String(),
+				xrand.RandomString(128),
+			}, "/"),
+		},
 	}
 
 	numMetadataCalls := 0
-	metadata := func(sessionID uuid.UUID) *Session {
+	data := func(sessionID uuid.UUID) *testData {
 		numMetadataCalls++
-		session.SessionID = sessionID.String()
-		return session
+		return session.Data
 	}
 
 	for i := 0; i < 10; i++ {
-		s.ReportStats(sessionID, 123, 456, metadata)
+		s.ReportStats(sessionID, 123, 456, data)
 	}
 
 	// Metadata must be called 1 time for the same SessionID
@@ -51,33 +57,32 @@ func TestService(t *testing.T) {
 	assert.Equal(t, 1, len(reports))
 	report := reports[0]
 	assert.Equal(t, sessionID.String(), report.SessionID)
-	assert.Equal(t, session.InstallationID, report.InstallationID)
-	assert.Equal(t, session.UserID, report.UserID)
+	assert.Equal(t, session.Data.InstallationID, report.Data.InstallationID)
+	assert.Equal(t, session.Data.UserID, report.Data.UserID)
 	assert.Equal(t, uint64(123)*10, report.DeltaRx)
 	assert.Equal(t, uint64(456)*10, report.DeltaTx)
 	assert.True(t, report.DeltaT > 0)
 }
 
 func TestServiceEmptyData(t *testing.T) {
-	var reports []*Report
-	s, err := New(time.Second, func(report *Report, extra Extra) {
+	var reports []*Report[testData]
+	s, err := New(time.Second, func(report *Report[testData], extra Extra) {
 		reports = append(reports, report)
 		assert.Equal(t, Extra{"a": "b"}, extra)
 	}, Extra{"a": "b"})
 	assert.NoError(t, err)
 
 	sessionID := uuid.New()
-	session := &Session{}
+	session := &Session[testData]{}
 
 	numMetadataCalls := 0
-	metadata := func(sessionID uuid.UUID) *Session {
+	data := func(sessionID uuid.UUID) *testData {
 		numMetadataCalls++
-		session.SessionID = sessionID.String()
-		return session
+		return session.Data
 	}
 
 	for i := 0; i < 10; i++ {
-		s.ReportStats(sessionID, 123, 456, metadata)
+		s.ReportStats(sessionID, 123, 456, data)
 	}
 
 	// Metadata must be called 1 time for the same SessionID
@@ -88,8 +93,8 @@ func TestServiceEmptyData(t *testing.T) {
 	assert.Equal(t, 1, len(reports))
 	report := reports[0]
 	assert.Equal(t, sessionID.String(), report.SessionID)
-	assert.Empty(t, report.InstallationID)
-	assert.Empty(t, report.UserID)
+	assert.Empty(t, report.Data.InstallationID)
+	assert.Empty(t, report.Data.UserID)
 	assert.Equal(t, uint64(123)*10, report.DeltaRx)
 	assert.Equal(t, uint64(456)*10, report.DeltaTx)
 	assert.True(t, report.DeltaT > 0)
